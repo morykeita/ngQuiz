ae4691a3d17e88615fabbfb82622430f4fcae11c
morykeita <morykeita@protonmail.com>
2017-05-25 23:04:59 -0400

    create a dataService, a factory which holds all my datas and can be injected into different controllers

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -3,9 +3,9 @@
  */
 (function () {
     angular.module('turtleFacts').controller("quizCtrl",QuizController);
-    QuizController.$inject=['quizMetrics'];
+    QuizController.$inject=['quizMetrics','DataService'];
 
-    function QuizController (quizMetrics) {
+    function QuizController (quizMetrics,DataService) {
          var vm=this;
          vm.quizMetrics=quizMetrics;
 
